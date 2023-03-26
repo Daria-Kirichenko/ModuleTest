8f96e522b2fdd6370d1cbbd6bf5e54430e9490f2
Daria <darakirichenko@gmail.com>
2023-03-26 22:12:51 +0300

    Specified array

--- ProgramCode/Program.cs
+++ ProgramCode/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+string[] array = {"100", "hello", "1", ":)"};
+Console.WriteLine(string.Join(", ", array));
